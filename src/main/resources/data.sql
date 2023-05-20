be0d6039e1917bb5ef1f2d8a5ecfdbf169404e1a
Rajkumar-Marimuthu <rajkumartechgy@gmail.com>
2023-05-20 11:20:15 +0100

    use of JPA

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user_details (id,birth_date,name) values (101,current_date,'Raj');
+insert into user_details (id,birth_date,name) values (102,current_date,'Kumar');
+insert into user_details (id,birth_date,name) values (103,current_date,'Mari');
+insert into user_details (id,birth_date,name) values (104,current_date,'Muthu');
+commit;
